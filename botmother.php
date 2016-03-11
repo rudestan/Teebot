b152bb0e621c9d0a764143ba68e29730c6b44994
Stanislav Drozdov <stanislav.drozdov@westwing.de>
2016-03-11 13:07:30 +0100

    Bot mother improvements

--- botmother.php
+++ botmother.php
@@ -10,8 +10,8 @@ class BotMother
     ];
 
     protected $commandMethodMapper = [
-        'createbot' => 'commandBotCreate',
-        'help'   => 'help'
+        'create-bot' => ['commandBotCreate', ['b:bot', 't:token', 'd:dir']],
+        'help'       => ['help', []],
     ];
 
     public function __construct()
@@ -26,7 +26,7 @@ class BotMother
             exit;
         }
 
-        $command = $opts['c'] ?? $opts['command'] ?? null;
+        $command = $opts['r'] ?? $opts['run'] ?? null;
 
         try {
             if (!$command) {
@@ -39,8 +39,11 @@ class BotMother
                 throw new Fatal('Unknown command!');
             }
 
-            echo "OK!\n";
+            $args = $this->getMethodArgs($method);
 
+
+
+            call_user_func_array([$this, $method], ['1','2','3']);
         } catch (Fatal $e) {
             echo $e->getMessage();
             exit;
@@ -50,23 +53,36 @@ class BotMother
     protected function help()
     {
         echo "
-        Bot mother CLI utility will help you easily and quickly create Bot or Command for existing bot. Please use
-        the following arguments:
+Bot mother CLI utility will help you easily and quickly create Bot or Command for existing bot. Please use
+the following arguments:
 
-        -r, --run [command_name] : Command name - required parameter. Could have the following values: create-bot, create-command
-        -b, --bot [bot_name]     : The name of the bot that you want to create
-        -c, --command [name]     : The name of the command that you want to create
-        -t, --token [value]      : Token for the bot creation
-        -d, --dir [dir]          : Bot's root directory (absolute path)\n\n";
+-r, --run [command_name] : Command name - required parameter. Could have the following values: create-bot, create-command
+-b, --bot [bot_name]     : The name of the bot that you want to create
+-c, --command [name]     : The name of the command that you want to create
+-t, --token [value]      : Token for the bot creation
+-d, --dir [dir]          : Bot's root directory (absolute path)\n\n";
     }
 
     protected function getMethod($command) {
-        return $this->commandMethodMapper[$command] ?? null;
+        $method = $this->commandMethodMapper[$command] ?? null;
+
+        if (!$method || !method_exists($this, $method)) {
+            return null;
+        }
+
+
+        return $method;
     }
 
-    protected function commandBotCreate($name, $token, $dir)
-    {
+    protected function getMethodArgs($methodName) {
 
+    }
+
+    protected function commandBotCreate($botName, $token, $dir)
+    {
+        echo "\nCreating a new bot.\n";
+
+        echo $botName." - ".$token." - ".$dir;
     }
 }
 
